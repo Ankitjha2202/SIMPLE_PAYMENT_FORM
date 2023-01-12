99438964f994a04dba883d52bbfcd67a822ed418
Ankit Jha <74873978+Ankitjha2202@users.noreply.github.com>
2023-01-12 18:22:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ app.get("/",function(req,res){
    res.sendFile(__dirname+"/summary.html");
 });
 
-app.get("/details.html",function(req,res){
-    res.sendFile(__dirname+"/details.html");
- });
+// app.get("/details.html",function(req,res){
+//     res.sendFile(__dirname+"/details.html");
+//  });
  
- app.get("/payments.html",function(req,res){
-    res.sendFile(__dirname+"/payments.html");
- });
+//  app.get("/payments.html",function(req,res){
+//     res.sendFile(__dirname+"/payments.html");
+//  });
 
- app.get("/success.html",function(req,res){
-   res.sendFile(__dirname+"/success.html");
-});
+//  app.get("/success.html",function(req,res){
+//    res.sendFile(__dirname+"/success.html");
+// });
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
