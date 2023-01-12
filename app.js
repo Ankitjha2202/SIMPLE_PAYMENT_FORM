4f44baad63f2a9c334404fcc1fcf1778d0450f40
Ankit Jha <74873978+Ankitjha2202@users.noreply.github.com>
2023-01-12 18:15:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+
+const app = express();
+
+app.get("/",function(req,res){
+   res.sendFile(__dirname+"/summary.html");
+});
+
+app.get("/details.html",function(req,res){
+    res.sendFile(__dirname+"/details.html");
+ });
+ 
+ app.get("/payments.html",function(req,res){
+    res.sendFile(__dirname+"/payments.html");
+ });
+
+ app.get("/success.html",function(req,res){
+   res.sendFile(__dirname+"/success.html");
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
